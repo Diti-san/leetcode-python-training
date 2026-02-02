feedce3004f3d44bcf9391d15bd4886fac2b05a9
TienDuy <tienmuongioi@gmail.com>
2026-02-02 17:40:55 +0700

    Delete 1_Two_Sum.py

--- medium/1_Two_Sum.py
+++ medium/1_Two_Sum.py
@@ -1,16 +1,1 @@
-"""
-Problem: 1. TwoSum
-Link: https://leetcode.com/problems/two-sum/
-Difficulty: Medium
-Topic: Array, Hash Table
-Time Complexity: O(n)
-Space Complexity: O(n)
-"""
-class Solution(object):
-    def twoSum(self, nums, target):
-        seen = {}
-        for i in range(len(nums)):
-            remain = target - nums[i]
-            if remain in seen:
-                return [seen[remain], i]
-            seen[nums[i]] = i
+
