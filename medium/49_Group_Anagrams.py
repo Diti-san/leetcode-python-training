9bfdf48ee847f04797ebf5ecd4c8e5f7a5591858
TienDuy <tienmuongioi@gmail.com>
2026-02-02 17:11:42 +0700

    Create 49_Group_Anagrams.py

---
+++ medium/49_Group_Anagrams.py
@@ -0,0 +1,20 @@
+"""
+Problem: 49. Group Anagrams
+Link: https://leetcode.com/problems/add-two-numbers/
+Difficulty: Medium
+Topic: Linked List, Math
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs):
+        groups = defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+            groups[key].append(s)
+
+        return list(groups.values())
