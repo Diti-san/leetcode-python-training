49340889607ba5fbc866d50bd41c4839f04e73fd
TienDuy <tienmuongioi@gmail.com>
2026-02-03 10:42:11 +0700

    Create 709_To_Lower_Case.py

---
+++ easy/709_To_Lower_Case.py
@@ -0,0 +1,11 @@
+"""
+Problem: 709. To Lower Case
+Link: https://leetcode.com/problems/to-lower-case
+Difficulty: Easy
+Topic: String
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+class Solution:
+    def toLowerCase(self, s):
+        return s.lower()
